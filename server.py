9a52c13dc96dd8d05a0396c7aa6a07337dea19c0
Justin Dray <justin@dray.be>
2015-04-26 07:28:31 +0000

    Fixed imports and made all returns json

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python2
 
-import json
-import oauth2client.client
-import oauth2client.multistore_file
-from flask import Flask, request, abort, jsonify, session, redirect
-from oauth2client.client import OAuth2WebServerFlow, TokenRevokeError
-from oauth2client.multistore_file import get_credential_storage
-import requests
 import httplib2
+import json
+from flask import Flask, request, abort, jsonify, session, redirect
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.multistore_file import get_credential_storage
 
 import youtube_to_gmusic
 
@@ -27,65 +24,6 @@ youtube_to_gmusic.VERBOSE = True
 app = Flask(__name__)
 app.secret_key = app_secret_key
 
-
-
-#@app.route("/download", methods=['POST'])
-#def download():
-#    if not request.json or 'link' not in request.json:
-#        abort(400)
-#    try:
-#        # youtube_to_gmusic.process_link(request.json['link'])
-#        return jsonify({}), 201
-#    except Exception as e:
-#        return jsonify({'error': e.message}), 500
-
-
-@app.route("/process", methods=['POST', 'GET'])
-def process():
-    if 'user' not in session or not session['user']:
-        session['redirect'] = True
-        session['request'] = request.form
-        return redirect('/login', code=301)
-    if session['redirect']:
-        print('%r' % session['request'])
-        session['redirect'] = False
-        data = session['request']
-    else:
-        data = request.form
-    if 'link' not in data:
-        return 'No link was specified!', 400
-        abort(400)
-
-    store = get_credential_storage(credential_store,
-                                   session['user'],
-                                   oauth_app_creds['user_agent'],
-                                   oauth_app_creds['scope'])
-    credentials = store.get()
-    if not credentials:
-        return jsonify({'result': 'Error',
-                        'details': 'No user credentials found'})
-
-    try:
-        youtube_to_gmusic.process_link(data['link'], credentials=credentials)
-        return jsonify({'result': 'Success',
-                        'details': None}), 200
-    except Exception as e:
-        return jsonify({'result': 'Error',
-                        'details': e.message}), 400
-
-
-#@app.route("/search", methods=['POST'])
-#def search():
-#    if not request.json or 'search' not in request.json:
-#        abort(400)
-#    try:
-#        youtube_to_gmusic.process_search(request.json['search'])
-#        return jsonify({}), 201
-#    except Exception as e:
-#        return jsonify({'error': e.message}), 500
-
-
-# Oauth Testing
 oauth_app_creds = {
     "client_secret": client_secret,
     "redirect_uri": "http://afzelia.dray.be:5000/oauth2callback",
@@ -102,79 +40,101 @@ authorize_url = flow.step1_get_authorize_url()
 credential_store = 'credentials'
 
 
-def is_valid_state(state):
-    return True
+@app.route("/process", methods=['POST', 'GET'])
+def process():
+    if 'user' not in session or not session['user']:
+        session['redirect'] = True
+        session['request'] = request.form
+        return redirect('/login', code=301)
+    else:
+        store = get_credential_storage(credential_store,
+                                       session['user'],
+                                       oauth_app_creds['user_agent'],
+                                       oauth_app_creds['scope'])
+        credentials = store.get()
 
+    if not credentials:
+        return jsonify({'error': 'No user credentials found'}), 500
 
-@app.route("/test", methods=['POST'])
-def test():
-    return '%r' % request.form['test']
+    if session['redirect']:
+        print('%r' % session['request'])
+        session['redirect'] = False
+        data = session['request']
+    else:
+        data = request.form
+    if 'link' not in data and 'search' not in data:
+        return jsonify({'error': 'No link or search term was specified!'}), 400
+        abort(400)
 
-
-@app.route("/get_session")
-def get_session():
-    return '%r' % session
+    try:
+        if 'link' in data:
+            youtube_to_gmusic.process_link(data['link'], credentials=credentials)
+        elif 'search' in data:
+            youtube_to_gmusic.process_search(data['search'], credentials=credentials)
+        else:
+            raise(Exception("This shouldn't happen!"))
+        return jsonify({}), 200
+    except Exception as e:
+        return jsonify({'error': e.message}), 400
 
 
 @app.route("/logout")
 def logout():
     if 'user' in session:
         session.clear()
-        text = "You've been logged out!"
+        out = "You've been logged out!"
     else:
-        text = "You were already logged out!"
-    return text
+        out = "You were already logged out!"
+    return jsonify({'details': out})
 
 
 @app.route("/login")
 def login():
     if 'user' in session and session['user']:
-        out = "Hi %s. You're already logged in" % session['user']
+        return jsonify({'details': "Hi %s. You're already logged in" % session['user']}), 200
     else:
-        out = redirect(authorize_url, code=301)
-    return out
+        return redirect(authorize_url, code=301)
 
 
 @app.route("/oauth2callback")
 def oauth2callback():
     error = request.args.get('error', '')
     if error:
-        return 'Error: ' + error
+        return jsonify({'error': error})
 
-    state = request.args.get('state', '')
-    if not is_valid_state(state):
-        # Request not started by us?
-        abort(403)
+    try:
+        credentials = flow.step2_exchange(code=request.args)
+        user = credentials.id_token['email']
+        store = get_credential_storage(credential_store,
+                                       user,
+                                       oauth_app_creds['user_agent'],
+                                       oauth_app_creds['scope'])
+        old_credentials = store.get()
+        if old_credentials:
+            ort = old_credentials.refresh_token
+            nrt = old_credentials.refresh_token
+            credentials.refresh_token = ort if not nrt else nrt
 
-    credentials = flow.step2_exchange(code=request.args)
-    user = credentials.id_token['email']
-    store = get_credential_storage(credential_store,
-                                   user,
-                                   oauth_app_creds['user_agent'],
-                                   oauth_app_creds['scope'])
-    old_credentials = store.get()
-    if old_credentials:
-        ort = old_credentials.refresh_token
-        nrt = old_credentials.refresh_token
-        credentials.refresh_token = ort if not nrt else nrt
+        # Clean up old credentials before writing new ones.
+        # For some reason it sometimes ends up with duplicates?
+        store.delete()
+        store.put(credentials)
+        http = httplib2.Http()
 
-    # Clean up old credentials before writing new ones.
-    # For some reason it sometimes ends up with duplicates?
-    store.delete()
-    store.put(credentials)
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-    credentials.refresh(http)
+        # Check that the credentials we have are correct
+        http = credentials.authorize(http)
+        credentials.refresh(http)
+    except Exception as e:
+        if 'invalid_grant' in e.message:
+            code = 500
+        else:
+            code = 400
+        return jsonify({'error': e.message}), code
+
     session['user'] = user
     if 'redirect' in session and session['redirect']:
         return redirect('/process', code=301)
     return redirect('/login', code=301)
-
-
-# Check if credentials are stored by email address
-# if they aren't, redirect to login. (is there a way to redirect again after?)
-# if they exist, attempt to process link
-# if they are no longer valid, redirect back to login
 
 
 if __name__ == "__main__":
