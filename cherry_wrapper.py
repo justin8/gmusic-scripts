868bc47a8554aa506aeed2d9ecf888e28af98a1c
Justin Dray <justin@dray.be>
2015-04-26 09:26:47 +0000

    Fixed requirements file and added cherry py wrapper

---
+++ cherry_wrapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+from cherrypy import wsgiserver
+from server import app
+
+d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
+server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 5000), d)
+
+if __name__ == '__main__':
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
